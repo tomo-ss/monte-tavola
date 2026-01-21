fbb6fbbd813e5c8d284b067e7a44f7226c1c9d37
tomo-ss <mssp.0321@outlook.jp>
2026-01-21 02:02:30 +0000

    管理画面：エラー表示UIを統一

--- resources/views/admin/holiday/create.blade.php
+++ resources/views/admin/holiday/create.blade.php
@@ -7,16 +7,17 @@
     休業日登録 
 </h1>
 
-    {{-- バリデーションエラー --}}
+    {{-- エラー表示 --}}
     @if ($errors->any())
-        <div class="mb-6 rounded border border-red-200 bg-red-50 px-4 py-3 text-sm text-red-700">
-            <ul class="list-disc pl-5 space-y-1">
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
+    <div class="mb-6 rounded-xl border border-red-200 bg-red-50 px-5 py-4 text-sm text-red-700">
+        <ul class="list-disc pl-5 space-y-1">
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+        </ul>
+    </div>
     @endif
+
 
     {{-- 登録フォーム --}}
     <form
--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -10,14 +10,15 @@
 
     {{-- エラーメッセージ --}}
 @if ($errors->any())
-    <div class="mb-6 rounded bg-red-100 p-4 text-red-700">
-        <ul class="list-disc pl-5">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+  <div class="mb-6 rounded-xl border border-red-200 bg-red-50 px-5 py-4 text-sm text-red-700">
+    <ul class="list-disc pl-5 space-y-1">
+      @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
 @endif
+
 
 
     {{-- 投稿フォーム --}}
